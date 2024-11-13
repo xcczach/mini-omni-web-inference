430106123ddd19a018aea2220b91598b75760e7d
Yifan Yang <yifanyeung@sjtu.edu.cn>
2024-11-13 05:24:09 +0000

    v1

---
+++ main.py
@@ -0,0 +1,62 @@
+from ml_web_inference import expose, Request, StreamingResponse, get_proper_device, get_model_size_mb
+import torch
+import io
+import argparse
+import torchaudio
+from inference import OmniInference
+import tempfile
+import numpy as np
+
+client = None
+device = None
+model_size_mb = 3500
+
+async def inference(request: Request) -> StreamingResponse:
+    data = await request.json()
+    sample_rate = data["sample_rate"]
+    audio_data = data["audio_data"]
+    with tempfile.NamedTemporaryFile(suffix=".wav") as f:
+        torchaudio.save(f, torch.tensor(audio_data).unsqueeze(0), sample_rate, format="wav")
+        chunk_generator = client.run_AT_batch_stream(f.name)
+        chunks = []
+        for chunk in chunk_generator:
+            chunk_audio_data = np.frombuffer(chunk, dtype=np.int16)
+            chunk_audio_data = chunk_audio_data.reshape(-1, 1)
+            chunks.append(chunk_audio_data)
+
+    result_arr = np.concatenate(chunks, axis=0).astype(np.int16)
+    result_arr = result_arr.transpose(1, 0)
+    result = io.BytesIO()
+    torchaudio.save(result, torch.tensor(result_arr), 24000, format="wav")
+    result.seek(0)
+    return StreamingResponse(result, media_type="application/octet-stream")
+
+
+def init():
+    global client, device
+    device = get_proper_device(model_size_mb)
+    client = OmniInference('./checkpoint', f"cuda:{device}")
+    client.warm_up()
+
+def hangup():
+    global client
+    del client
+    torch.cuda.empty_cache()
+
+
+if  __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=9234)
+    parser.add_argument("--api-name", type=str, default="miniomni")
+    parser.add_argument("--hangup-timeout-sec", type=int, default=900)
+    parser.add_argument("--hangup-interval-sec", type=int, default=60)
+    args = parser.parse_args()
+    expose(
+        args.api_name,
+        inference,
+        port=args.port,
+        hangup_timeout_sec=args.hangup_timeout_sec,
+        hangup_interval_sec=args.hangup_interval_sec,
+        init_function=init,
+        hangup_function=hangup,
+    )
---
+++ test_client.py
@@ -0,0 +1,25 @@
+import requests
+import soundfile as sf
+import io
+
+url = "http://localhost:9234/miniomni"
+audio_data, sample_rate = sf.read("en_sample.wav")
+data = {"sample_rate": sample_rate, "audio_data": audio_data.tolist()}
+
+response = requests.post(url, json=data, stream=True)
+
+if response.status_code == 200:
+    audio_chunks = []
+
+    buffer = io.BytesIO()
+    for chunk in response.iter_content(chunk_size=4096):
+        buffer.write(chunk)
+
+    buffer.seek(0)
+
+    audio_data, sample_rate = sf.read(buffer)
+    sf.write("output_audio.wav", audio_data, sample_rate)
+
+    print("Audio saved at output_audio.wav")
+else:
+    print(f"Bad request：{response.status_code}")
